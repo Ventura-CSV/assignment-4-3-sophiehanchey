64de9959309d6527f4f089836a9df4e5d9d8c349
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 22:35:07 -0700

    initial code, set number greater than two instead of zero so am missing one remainder

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def main():
     number = int(input('Enter your input: '))
     result = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    # do the division 
+    while number >= 2:
+        result.append(number % 2)
+        number = number // 2
 
     print(*result)
     ########################################
